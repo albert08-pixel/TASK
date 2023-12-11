8611d895249716abbe3770b27ab15729b11de0f1
albert08-pixel <64290305+albert08-pixel@users.noreply.github.com>
2023-12-10 19:51:24 -0800

    Comments

--- Task.java
+++ Task.java
@@ -1,40 +1,40 @@
 package main.java;
 
-class Task {
-    private String taskName;
-    private long dueTime;
-    private String description;
-
-    public Task(String taskName, long dueTime,  String description) {
-        this.taskName = taskName;
-        this.dueTime = dueTime;
-        this.description = description;
-    }
-
-    public String getTaskName() {
-        return taskName;
-    }
-
-    public long getDueTime() {
-        return dueTime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void updateTaskName(String taskName) {
-        this.taskName = taskName;
-        System.out.println("Successfully Changed Task Name!");
-    }
-
-    public void updateDueTime(long dueTime) {
-        this.dueTime = dueTime;
-        System.out.println("Successfully Changed Task Due Time!");
-    }
-
-    public void updateDescription(String description) {
-        this.description = description;
-        System.out.println("Successfully Changed Task Description!");
-    }
-}
+ class Task {
+     private String taskName;
+     private long dueTime;
+     private String description;
+    // Constructor for creating a new task
+     public Task(String taskName, long dueTime,  String description) {
+         this.taskName = taskName;
+         this.dueTime = dueTime;
+         this.description = description;
+     }
+     // Retrieving the task name
+     public String getTaskName() {
+         return taskName;
+     }
+     // Retrieving the due time
+     public long getDueTime() {
+         return dueTime;
+     }
+     // Retrieving the task description
+     public String getDescription() {
+         return description;
+     }
+     // Method for updating the task name
+     public void updateTaskName(String taskName) {
+         this.taskName = taskName;
+         System.out.println("Successfully Changed Task Name!");
+     }
+     // Method for updating the due time
+     public void updateDueTime(long dueTime) {
+         this.dueTime = dueTime;
+         System.out.println("Successfully Changed Task Due Time!");
+     }
+     // Method for updating the task description
+     public void updateDescription(String description) {
+         this.description = description;
+         System.out.println("Successfully Changed Task Description!");
+     }
+ }
